125a35a03f1c76aca9fa914ba70a38b284784085
Jacob LeGrone <git@jacob.work>
2025-09-09 12:40:56 -0400

    Remove types_test.go

--- api/v1alpha1/types_test.go
+++
@@ -1,4 +0,0 @@
-package v1alpha1
-
-// Note: Default behavior is comprehensively tested in temporalworker_webhook_test.go
-// This file is kept for potential future type-specific tests that don't belong in webhook tests
