889261aac9455cf0234c88ee044db9e46fd6f718
Carly de Frondeville <cdefrondeville@berkeley.edu>
2026-04-30 13:29:50 -0700

    rename occurrences in demo

--- internal/demo/scripts/move_pinned_workflows.sh
+++ internal/demo/scripts/move_pinned_workflows.sh
@@ -6,7 +6,7 @@
 #  $ move_pinned_workflows.sh staging/helloworld v1 v2
 
 temporal workflow update-options \
-  --query "TemporalWorkerDeploymentVersion='$1:$2' AND ExecutionStatus='Running'" \
+  --query "WorkerDeploymentVersion='$1:$2' AND ExecutionStatus='Running'" \
   --versioning-override-behavior pinned \
   --versioning-override-deployment-name "$1" \
   --versioning-override-build-id "$3"
@@ -20,4 +20,4 @@ TARGET_DEPLOYMENT_VERSION_ENCODED=$(printf '%s' "$1:$3" | sed 's/:/%3A/g; s/\//%
 
 echo ""
 echo "You can view the status of the impacted workflows at:"
-echo "https://cloud.temporal.io/namespaces/${NAMESPACE}/workflows?query=BuildIds+IN+%28%22${SOURCE_BUILD_ID_ENCODED}%22%29+AND+TemporalWorkerDeploymentVersion+%3D+%22${TARGET_DEPLOYMENT_VERSION_ENCODED}%22"
+echo "https://cloud.temporal.io/namespaces/${NAMESPACE}/workflows?query=BuildIds+IN+%28%22${SOURCE_BUILD_ID_ENCODED}%22%29+AND+WorkerDeploymentVersion+%3D+%22${TARGET_DEPLOYMENT_VERSION_ENCODED}%22"
