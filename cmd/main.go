32d0516d6cd3a945b63b2889208e92e1d288e1a6
Jay Pipes <jaypipes@gmail.com>
2026-07-28 04:58:53 -0400

    Update cmd/main.go
    Co-authored-by: Jacob LeGrone <jlegrone@users.noreply.github.com>

--- cmd/main.go
+++ cmd/main.go
@@ -138,7 +138,7 @@ func main() {
 		setupLog.Error(err, "unable to create webhook", "webhook", "WorkerResourceTemplate")
 		os.Exit(1)
 	}
-	if err = (&temporaliov1alpha1.WorkerDeployment{}).SetupWebhookWithManager(mgr); err != nil {
+	if err := (&temporaliov1alpha1.WorkerDeployment{}).SetupWebhookWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create webhook", "webhook", "WorkerDeployment")
 		os.Exit(1)
 	}
